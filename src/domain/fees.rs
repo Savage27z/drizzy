d42cb8585cc3932a5e0104609e60331d3bc2510c
Savage27z <freakysavage23@gmail.com>
2026-08-22 06:30:41 +0100

    wip(fees.rs): incremental update

--- src/domain/fees.rs
+++ src/domain/fees.rs
@@ -58,6 +58,7 @@ impl AutomaticFeePolicy {
         let buffer_bump_bps = self.replacement_bump_bps.min(MAX_BASE_FEE_GROWTH_BPS);
         let grown_buffer = multiply_ceil(buffer, buffer_bump_bps)?;
 
+        let max_fee_per_gas = max_priority_fee_per_gas
     }
 }
 
